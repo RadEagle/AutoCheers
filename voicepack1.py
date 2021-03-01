b75b5264d9ab817c909bb2a37fde8715e2fb277c
Jonathan Chau <31170739+RadEagle@users.noreply.github.com>
2021-03-01 08:43:55 -0800

    Add files via upload

---
+++ voicepack1.py
@@ -0,0 +1,21 @@
+import os
+import numpy as np
+import pyglet
+
+# Let pyglet access all files inside directory
+pyglet.options['search_local_libs'] = True
+
+# Get a random value
+file_count = len(os.listdir('keqing'))
+x = int(1 + np.random.rand() * file_count)
+
+# Load media
+audio_file = 'keqing/{}.wav'.format(x)
+audio = pyglet.media.load(audio_file)
+player = pyglet.media.Player()
+player.queue(audio)
+player.play()
+
+# Run once, then stop
+player.on_eos = pyglet.app.exit
+pyglet.app.run()
