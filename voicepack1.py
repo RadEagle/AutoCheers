064137cdfc7135026304f015e7269a4f1c712c4c
Jonathan Chau <31170739+RadEagle@users.noreply.github.com>
2021-03-03 08:23:29 -0800

    Update voicepack1.py
    Added feature to ensure the 3 recent voice clips won't be played again

--- voicepack1.py
+++ voicepack1.py
@@ -6,6 +6,14 @@ import pyglet
 pyglet.options['search_local_libs'] = True
 audio_dir = os.listdir('keqing')
 
+# Initialize a history list
+try:
+    history_file = open('history1.txt', 'r+')
+    history_list = history_file.read().split("\n")
+except FileNotFoundError:
+    history_file = open('history1.txt', 'w')
+    history_list = []
+
 # Get .wav file count
 waves = []
 for filename in audio_dir:
@@ -14,6 +22,20 @@ for filename in audio_dir:
 
 # Get a random value
 x = random.choice(waves)
+while x in history_list:
+    x = random.choice(waves)
+
+# Append x to history_list
+# This ensures the 3 recent clips won't be played again
+history_list.append(x)
+if len(history_list) > 3:
+    history_list.pop(0)
+
+# Print to history file
+history_file.seek(0)
+history_file.write('\n'.join(history_list))
+history_file.truncate()
+history_file.close()
 
 # Load media
 audio_file = 'keqing/{}'.format(x)
